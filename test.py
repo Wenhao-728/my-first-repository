34b1e6d82c1ef6685442bf1e5fb3865780787949
Sun Wenhao <wenhao.sun@lazada.com>
2021-01-16 00:44:05 +0800

    version 3

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 def main() -> object:
-    print("Hello World!")
+    print("version 1")
     print("version 2")
-    print("version 3 dev")
+    print("version 3")
 
 
 if __name__ == "__main__":
+    pirnt('This is the learning notes of Sun Wenhao')
     main()
